c90d407f727cb12d306a9b1a0666fd192e30657a
peter <peterkameel.93@gmail.com>
2022-11-23 18:49:36 +0200

    add navbar and router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 
+import AllMeetupsPage from './pages/AllMeetups';
+import FavoritesPage from './pages/Favorites';
+import NewMeetupsPage from './pages/NewMeetup';
+import MainNavigation  from './components/layout/MainNavigation';
 
 function App() {
   return (
-    <div>
+    <Router>
+      <MainNavigation />
+
+      <Routes>
+        <Route path='/' element={<AllMeetupsPage /> } />
+        <Route path='/new-meetups' element={<NewMeetupsPage />} />
+        <Route path='/favorites' element={<FavoritesPage /> } />
+      </Routes>
      
-    </div>
+    </Router>
   );
 }
 
---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,19 @@
+import {Link} from 'react-router-dom'
+
+function MainNavigation(){
+    return(
+        <header>
+            <div>React Meetups</div>
+            <nav>
+                <ul>
+                    <li> <Link to='/'>All Meetups</Link> </li>
+                    <li> <Link to='/new-meetups'>Add New Meetups</Link> </li>
+                    <li> <Link to='/favorites'>Favorites</Link> </li>
+                </ul>
+            </nav>
+        </header>
+    )
+
+}
+
+export default MainNavigation;
