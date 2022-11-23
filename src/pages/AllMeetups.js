a873b274ee8e88ba25a462a7fa1747971fedde70
peter <peterkameel.93@gmail.com>
2022-11-23 21:21:13 +0200

    working on all meetups page

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,8 +1,27 @@
+import MeetupList from "../components/meetups/MeetupList";
+const DUMMY_DATA=[
+    {
+        id:'m1',
+        title:'This the first meetups',
+        image:'https://www.shutterstock.com/image-vector/meet-on-speech-bubble-600w-1618832668.jpg',
+        description:'description of the first meetups'
+    },
+    {
+        id:'m2',
+        title:'This the second meetups',
+        image:'https://www.shutterstock.com/image-vector/meet-on-speech-bubble-600w-1618832668.jpg',
+        description:'description of the second meetups'
+    }
+]
+
 function AllMeetupsPage(){
     return(
-        <div><h1>All Meetings Page</h1></div>
+        <section>
+            <h1>All Meetups</h1>
+            <MeetupList meetups={DUMMY_DATA} />
+        </section>
     )
-
+ 
 }
 
 export default AllMeetupsPage;
