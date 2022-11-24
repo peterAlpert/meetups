c49b391448cf5f4ff26f5c3023487cd968faf0ef
peter <peterkameel.93@gmail.com>
2022-11-24 12:59:44 +0200

    get data from firebase using http request/fetch

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,27 +1,71 @@
+import { useState, useEffect } from "react";
 import MeetupList from "../components/meetups/MeetupList";
-const DUMMY_DATA=[
-    {
-        id:'m1',
-        title:'This the first meetups',
-        image:'https://www.shutterstock.com/image-vector/meet-on-speech-bubble-600w-1618832668.jpg',
-        description:'description of the first meetups'
-    },
-    {
-        id:'m2',
-        title:'This the second meetups',
-        image:'https://www.shutterstock.com/image-vector/meet-on-speech-bubble-600w-1618832668.jpg',
-        description:'description of the second meetups'
-    }
-]
+
 
 function AllMeetupsPage(){
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+    useEffect(()=>{
+        setIsLoading(true);
+        fetch('https://react-meetups-b7a98-default-rtdb.firebaseio.com/meetups.json')
+        
+        .then((res)=>{
+            return res.json();
+        })
+
+        .then((data)=>{
+            const meetups = [];
+
+            for(const key in data){
+                const meetup ={
+                    id:key,
+                    ...data[key]
+                };
+                meetups.push(meetup);
+            }
+
+            setIsLoading(false);
+            setLoadedMeetups(meetups);
+        });
+    },[]);
+
+
+    if(isLoading){
+        return(
+            <section>
+                <h1>Loading ....</h1>
+            </section>
+        )
+    }
+
     return(
         <section>
             <h1>All Meetups</h1>
-            <MeetupList meetups={DUMMY_DATA} />
+            <MeetupList meetups={loadedMeetups} />
         </section>
     )
  
 }
 
+
 export default AllMeetupsPage;
+
+
+
+
+
+// const DUMMY_DATA=[
+//     {
+//         id:'m1',
+//         title:'This the first meetups',
+//         image:'https://www.shutterstock.com/image-vector/meet-on-speech-bubble-600w-1618832668.jpg',
+//         description:'description of the first meetups'
+//     },
+//     {
+//         id:'m2',
+//         title:'This the second meetups',
+//         image:'https://www.shutterstock.com/image-vector/meet-on-speech-bubble-600w-1618832668.jpg',
+//         description:'description of the second meetups'
+//     }
+// ]
