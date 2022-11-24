d6858103bfc7576d922060f5fcbf339057b0a485
peter <peterkameel.93@gmail.com>
2022-11-24 11:41:36 +0200

    making a request to post data using firebase

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -3,7 +3,7 @@ import {useRef} from 'react'
 import Card  from '../ui/Card';
 import classes from './NewMeetupForm.module.css'
 
-function NewMeetupForm(){
+function NewMeetupForm(props){
     const titleInputRef = useRef();
     const imageInputRef = useRef();
     const addressInputRef = useRef();
@@ -22,11 +22,9 @@ function NewMeetupForm(){
             image:enteredImage,
             address:enteredAddress,
             description:enteredDescription
-
-
         }
-
         console.log(meetupData);
+        props.onAddmeetup(meetupData);
     }
 
     return(
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,20 @@
 import NewMeetupForm  from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage(){
+    function addMeetupHandler(meetupData){
+        fetch('https://react-meetups-b7a98-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method:'POST',
+                body: JSON.stringify(meetupData),
+                headers: { 'Content-Type' : 'application/json'}
+            }
+        )
+
+    }
     return(
         <section>
             <h1>Add New Meetups</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddmeetup={addMeetupHandler}/>
         </section>
     )
 
